4ca32e46d6616bae525b819be511ebee14e04437
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-20 01:28:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,31 +23,31 @@ end
 
 def remove_element_from_end_of_array(array)
   
-  create_an_array.pop
+  array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
   
-  create_an_array.shift
+  array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
   
-  create_an_array[1]
+  array[index_number]
   
 end
 
 def retrieve_first_element_from_array(array)
   
-  create_an_array[0]
+  array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
   
-  create_an_array[2]
+  array[2]
   
 end
 
